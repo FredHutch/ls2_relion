91a811cf63ef653c5a11f389dcd182269fd65821
Ben McGough <bmcgough@fredhutch.org>
2018-04-25 15:14:14 -0700

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
+# meant to be placed in the users' home directory
+# starts a new container or execs a new shell in a running container
+
+# the docker executable
+docker_cmd="nvidia-docker"
+
+# I use sudo, you may not
+sudo="sudo"
+
+# the image to use
+relion_container_img="fredhutch/ls2-relion:2.1"
+
 function connect_container() {
-  sudo nvidia-docker exec -ti \
-    relion /bin/bash
+  $sudo $docker_cmd exec -ti \
+    relion1 /bin/bash
 }
 
 function start_container() {
-  sudo nvidia-docker run -ti --rm \
+  $sudo $docker_cmd run -ti --rm \
     --net=host \
     -e DISPLAY=$DISPLAY \
     -e LD_LIBRARY_PATH=/usr/local/lib \
-    -v /home/ec2-user:/home/neo \
-    --name relion fredhutch/ls2-relion:2.1 /bin/bash -i
+    -v $CWD:/home/neo \
+    --name relion1 $relion_container_img /bin/bash -i
 }
 
 # check for running container
-sudo nvidia-docker ps | grep -q relion1
+$sudo $docker_cmd ps | grep -q relion1
 
 if [ "$?" -eq "0" ]
 then
