25d46f8ed24684b4a9770b8d0727deefa6800bf3
Ben McGough <bmcgough@fredhutch.org>
2018-04-25 15:14:30 -0700

    Update download_relion21_tutorial.sh

--- download_relion21_tutorial.sh
+++ download_relion21_tutorial.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# get tutorial PDF
-curl -s -L ftp://ftp.mrc-lmb.cam.ac.uk/pub/scheres/relion21_tutorial.pdf > relion21_tutorial.pdf
-
 # get tutorial data
 curl -L ftp://ftp.mrc-lmb.cam.ac.uk/pub/scheres/relion21_tutorial.tar.gz > relion21_tutorial.tar.gz
 tar -xzf relion21_tutorial.tar.gz
